cdac454c65b868e45490b61c1a21d53ca4dd54a3
Sai Rajesh Paleti <spaleti@evoketechnologies.com>
2021-10-26 17:08:22 +0530

    user-management: API integration and smaple json added

--- src/components/admin/UserManagement/UserDetailsDashboard.jsx
+++ src/components/admin/UserManagement/UserDetailsDashboard.jsx
@@ -1,7 +1,30 @@
 // common imports
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 const UserDetailsDashboard = () => {
+    const sampleResponse = {
+        "userDetails": {
+            "userName": 'Rajesh',
+            "email": "spaleti@evoketechnologies.com",
+            "userRole": 'Admin',
+            "team": "developer",
+            "dateCreated": "26-10-2021",
+            "contactNumber": "7995638533",
+            "status": "active"
+        }
+    };
+    const [userDetails, setUserDetails] = useState(sampleResponse);
+    useEffect(()=>{
+        getUserDetails();
+    }, []);
+
+    const getUserDetails = () => {
+        // commentd for future use-once api is created will integrated here
+    //     return axios.post(getApiUrl(`userDetails`))
+    // .then((res) => {
+    //      return setUserDetails(res)
+    // })
+    // .catch((err) => err);
+   }
  
   return (
     <div>
