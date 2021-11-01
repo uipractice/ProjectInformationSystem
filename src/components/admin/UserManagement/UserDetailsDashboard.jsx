637db9becd4e02764865de8eb09a043101e92dc5
Sai Rajesh Paleti <spaleti@evoketechnologies.com>
2021-11-01 15:48:30 +0530

    Initial code

--- src/components/admin/UserManagement/UserDetailsDashboard.jsx
+++ src/components/admin/UserManagement/UserDetailsDashboard.jsx
@@ -3,31 +3,36 @@ import React, { useState, useEffect } from 'react';
 import NavBar from '../NavBar';
 import Footer from '../Footer';
 import UserTable from '../UserManagement/UserTable';
-import './UserDetails.css'
+import './UserDetails.css';
+import AddUserModal from './AddUserModal';
 const UserDetailsDashboard = () => {
-    const sampleResponse = {
-        "userDetails": {
-            "userName": 'Rajesh',
-            "email": "spaleti@evoketechnologies.com",
-            "role": 'Admin',
-            "team": "developer",
-            "dateCreated": "26-10-2021",
-            "contactNumber": "7995638533",
-            "status": "active"
-        }
-    };
-    const [userDetails, setUserDetails] = useState(sampleResponse);
+    // const sampleResponse = {
+    //     "userDetails": {
+    //         "userName": 'Rajesh',
+    //         "email": "spaleti@evoketechnologies.com",
+    //         "role": 'Admin',
+    //         "team": "developer",
+    //         "dateCreated": "26-10-2021",
+    //         "contactNumber": "7995638533",
+    //         "status": "active"
+    //     }
+    // };
+   //const [userDetails, setUserDetails] = useState(sampleResponse);
+   const [showModal, setShowModal] = useState(false);
     useEffect(()=>{
-        getUserDetails();
+        // getUserDetails(sampleResponse);
     }, []);
 
-    const getUserDetails = () => {
+    //const getUserDetails = () => {
         // commentd for future use-once api is created will integrated here
     //     return axios.get(getApiUrl(`userDetails`))
     // .then((res) => {
     //      return setUserDetails(res)
     // })
     // .catch((err) => err);
+  // }
+   const openModal = () => {
+    setShowModal(true);
    }
  
   return (
@@ -37,8 +42,9 @@ const UserDetailsDashboard = () => {
            <h3>USER MANAGEMENT SYSTEM</h3>
            <button
             type='button'
-            className='btn work_btn work_btn_blue center modal-button'>Add User</button>
+            className='btn work_btn work_btn_blue center modal-button' onClick={openModal}>Add User</button>
        </div>
+       {showModal && <AddUserModal isOpen={showModal}/>}
        <Footer />
        <UserTable />
      </div>
